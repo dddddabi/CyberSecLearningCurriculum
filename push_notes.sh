ff1cf590ad3ed1e5ecbd873e0055ff4a44674fd5
dddddabi <davejared.gabuat10@gmail.com>
2026-07-15 23:19:29 +0800

    sync:  2026-07-15 23:19:29

---
+++ push_notes.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+LOG_FILE="scratchpad.md"
+
+clear 
+echo "===================================" 
+echo "     INPUT DAILY CYBERSEC LOG      " 
+echo "===================================" 
+read -r USER_INPUT
+
+if [ -z "$USER_INPUT" ]; then 
+	echo "Notice. EMpty input detected. Aborting."
+	sleep 2
+	exit 1
+fi
+
+echo -e "\n### $(date +'%Y-%m-%d %H:%M')\n* $USER_INPUT" >> "$LOG_FILE"
+
+git add .
+git commit -m "sync:  $(date +'%Y-%m-%d %H:%M:%S')"
+git push 
+
+echo "Notice. Remote synchronization complete."
+sleep 2
+
